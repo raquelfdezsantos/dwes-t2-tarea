5e3ae6199c5ee623c0819279db213ada47c7c89b
Raquel Fernandez <raquelfernandezsantos@alumnos.tuniversformacion.es>
2024-11-20 16:53:27 +0100

    6. Incluyo las clases necesarias para la gestión de alumnos, profesores y asignaturas

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestión del Centro Educativo</title>
+</head>
+<body>
+    <?php
+    require_once 'clases/Alumno.php';
+    require_once 'clases/Profesor.php';
+    require_once 'clases/Asignatura.php';
+
+
+    
+
+
+
+
+    ?>
+    
+    </body>
+    </html>    
